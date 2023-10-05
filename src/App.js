ee68ded2ab46d4f8cc651ff7d7e602103103ea2d
Oleksandr Moreka <gyzica40@gmail.com>
2023-10-05 18:57:47 +0000

    App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://16.16.213.168/api');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +22,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={fetchData}>Fetch Data</button>
         <a
           className="App-link"
           href="https://reactjs.org"
