65204e78c402f11d695c54632de7f6140f801f6c
S4nya228 <gyzica40@gmail.com>
2023-10-06 11:30:36 +0300

    change app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
     const fetchData = async () => {
     try {
-        const response = await fetch('/api');
+        const response = await fetch('/api/test');
         if (!response.ok) {
         throw new Error('Network response was not ok');
         }
