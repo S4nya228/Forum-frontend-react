0472256b3a252206e1df51ceb0cdbe1d70908d0b
S4nya228 <gyzica40@gmail.com>
2023-10-06 11:27:24 +0300

    change app.js

--- src/App.js
+++ src/App.js
@@ -2,38 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const fetchData = async () => {
+    const fetchData = async () => {
     try {
-      const response = await fetch('/api');
-      if (!response.ok) {
+        const response = await fetch('/api');
+        if (!response.ok) {
         throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      console.log(data);
+        }
+        const data = await response.json();
+        console.log(data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error);
     }
-  };
+    };
 
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload.
         </p>
         <button onClick={fetchData}>Fetch Data</button>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
-          Learn React
+            Learn React
         </a>
-      </header>
+        </header>
     </div>
-  );
+    );
 }
 
 export default App;
