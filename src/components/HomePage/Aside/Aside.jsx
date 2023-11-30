e693e65fa7627658e81b987af0ba780021a4fbff
S4nya228 <gyzica40@gmail.com>
2023-11-30 16:17:56 +0200

    added output groups on home page

--- src/components/HomePage/Aside/Aside.jsx
+++ src/components/HomePage/Aside/Aside.jsx
@@ -1,84 +1,60 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axiosInstance from '../../../api/axiosInstance'
 import '../Aside/Aside.scss'
-import { Link } from 'react-router-dom'
 
 const Aside = () => {
+	const [groups, setGroups] = useState([])
+	const [showAllGroups, setShowAllGroups] = useState(false)
+
+	useEffect(() => {
+		const fetchGroups = async () => {
+			try {
+				const response = await axiosInstance.get('/client/community')
+				setGroups(response.data.data)
+			} catch (error) {
+				console.log('Error:', error)
+			}
+		}
+		fetchGroups()
+	}, [])
+
+	const sortedGroups = groups
+		.slice()
+		.sort((a, b) => b.subcriber_count - a.subcriber_count)
+
+	const displayedGroups = showAllGroups
+		? sortedGroups
+		: sortedGroups.slice(0, 6)
+
 	return (
 		<div className="aside">
 			<div className="aside__container">
 				<div className="aside__title">Popular group</div>
 				<ul className="aside__list">
-					<li className="aside__item">
-						<div className="aside__icon-group">
-							<img src="/image/group_icon.svg" alt="icon group" />
-						</div>
-						<div className="aside__info-box">
-							<div className="aside__name">
-								<span>Ukraine</span>
+					{displayedGroups.map((group) => (
+						<li key={group.community_id} className="aside__item">
+							<div className="aside__icon-group">
+								<img src="/image/group_icon.svg" alt="icon group" />
 							</div>
-							<div className="aside__description">
-								<span>4,078,323 subscribers</span>
+							<div className="aside__info-box">
+								<div className="aside__name">
+									<span>{group.name}</span>
+								</div>
+								<div className="aside__description">
+									<span>{`${group.subcriber_count} subscribers`}</span>
+								</div>
 							</div>
-						</div>
-					</li>
-					<li className="aside__item">
-						<div className="aside__icon-group">
-							<img src="/image/group_icon.svg" alt="icon group" />
-						</div>
-						<div className="aside__info-box">
-							<div className="aside__name">
-								<span>Ukraine</span>
-							</div>
-							<div className="aside__description">
-								<span>7,348,323 subscribers</span>
-							</div>
-						</div>
-					</li>
-					<li className="aside__item">
-						<div className="aside__icon-group">
-							<img src="/image/group_icon.svg" alt="icon group" />
-						</div>
-						<div className="aside__info-box">
-							<div className="aside__name">
-								<span>Ukraine</span>
-							</div>
-							<div className="aside__description">
-								<span>10,178,323 subscribers</span>
-							</div>
-						</div>
-					</li>
-					<li className="aside__item">
-						<div className="aside__icon-group">
-							<img src="/image/group_icon.svg" alt="icon group" />
-						</div>
-						<div className="aside__info-box">
-							<div className="aside__name">
-								<span>Ukraine</span>
-							</div>
-							<div className="aside__description">
-								<span>41,078,323 subscribers</span>
-							</div>
-						</div>
-					</li>
-					<li className="aside__item">
-						<div className="aside__icon-group">
-							<img src="/image/group_icon.svg" alt="icon group" />
-						</div>
-						<div className="aside__info-box">
-							<div className="aside__name">
-								<span>Ukraine</span>
-							</div>
-							<div className="aside__description">
-								<span>4,238,323 subscribers</span>
-							</div>
-						</div>
-					</li>
+						</li>
+					))}
 				</ul>
 				<div className="aside__see-more">
-					<Link to="/group" className="aside__link">
-						See more
-					</Link>
+					<div
+						to="/group"
+						onClick={() => setShowAllGroups(!showAllGroups)}
+						className="aside__link"
+					>
+						{showAllGroups ? 'See less' : 'See more'}
+					</div>
 				</div>
 			</div>
 		</div>
