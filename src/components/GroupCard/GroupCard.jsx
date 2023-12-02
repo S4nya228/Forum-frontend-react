6628ac784802a798d82847cd5a14bb941d040c34
S4nya228 <gyzica40@gmail.com>
2023-12-02 16:51:33 +0200

    added functional subscribe on group

--- src/components/GroupCard/GroupCard.jsx
+++ src/components/GroupCard/GroupCard.jsx
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import '../GroupCard/GroupCard.scss'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import axiosInstance from '../../api/axiosInstance'
+import { setSubscribed } from '../../store/userSlise'
 
 const GroupCard = () => {
 	const token = useSelector((state) => state.auth.token)
 	const [groupData, setGroupData] = useState(null)
 	const navigate = useNavigate()
+	const dispatch = useDispatch()
 	const { postId } = useParams()
+	const subscribedData = useSelector(
+		(state) => state.user.subscriptions[postId]
+	)
+	const [isSubscribed, setIsSubscribed] = useState(subscribedData)
 
 	useEffect(() => {
 		const fetchGroupData = async () => {
@@ -36,11 +42,31 @@ const GroupCard = () => {
 		fetchGroupData()
 	}, [postId])
 
-	const handleJoinGroup = () => {
+	const handleJoinGroup = async () => {
 		if (token) {
-			console.log('вступив')
+			try {
+				const endpoint = isSubscribed
+					? `/client/community/${groupData.community_info.community_id}/unsubscribe`
+					: `/client/community/${groupData.community_info.community_id}/subscribe`
+
+				await axiosInstance.post(
+					endpoint,
+					{},
+					{
+						headers: {
+							Authorization: `Bearer ${token}`,
+							'Content-Type': 'application/json',
+						},
+					}
+				)
+				dispatch(
+					setSubscribed({ groupId: postId, isSubscribed: !isSubscribed })
+				)
+				setIsSubscribed(!isSubscribed)
+			} catch (error) {
+				console.error('Error:', error.response)
+			}
 		} else {
-			console.log('невступив:')
 			navigate('/login')
 		}
 	}
@@ -82,7 +108,9 @@ const GroupCard = () => {
 							</div>
 						</div>
 						<div className="card-group__join">
-							<button onClick={handleJoinGroup}>Join</button>
+							<button onClick={handleJoinGroup}>
+								{isSubscribed ? 'Unsubscribe' : 'Subscribe'}
+							</button>
 						</div>
 					</div>
 				</div>
--- src/store/userSlise.js
+++ src/store/userSlise.js
@@ -1,19 +1,28 @@
+// userSlice.js
+
 import { createSlice } from '@reduxjs/toolkit'
 
-const userSlise = createSlice({
+const userSlice = createSlice({
 	name: 'user',
 	initialState: {
 		user: null,
+		subscriptions: {},
 	},
 	reducers: {
 		setUser: (state, action) => {
 			state.user = action.payload.data
+			state.subscriptions = {}
+		},
+		setSubscribed: (state, action) => {
+			const { groupId, isSubscribed } = action.payload
+			state.subscriptions[groupId] = isSubscribed
 		},
 		clearUser: (state) => {
 			state.user = null
+			state.subscriptions = {}
 		},
 	},
 })
 
-export const { setUser, clearUser } = userSlise.actions
-export default userSlise.reducer
+export const { setUser, setSubscribed, clearUser } = userSlice.actions
+export default userSlice.reducer
